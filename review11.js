80d992f14392090285341d0fcca3e50d361bd573
Falade Joseph <faladejoseph400@gmail.com>
2025-12-23 15:40:37 +0100

    Encapsulate length property

--- review11.js
+++ review11.js
@@ -1,12 +1,14 @@
 class Square {
+    #length ;  // # sign makes properties private;
     constructor (length) {
-        this.length = length;
+        this.#length = length;
     };
     getPerimeter() {
-        let perimeter = 4 * this.length;
+        let perimeter = 4 * this.#length;
         return perimeter;
     }
 }
 
 let squareOne = new Square(4);
+squareOne.length = 5;
 console.log(squareOne.getPerimeter());
