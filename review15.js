ba209cda772f328d14b5329d4afe42d16da03084
Falade Joseph <faladejoseph400@gmail.com>
2025-12-24 17:06:01 +0100

    Use Map method to assign departments

---
+++ review15.js
@@ -0,0 +1,28 @@
+// Map method.
+ let student1 = {
+    name: "Victor",
+    regNo: "001"
+ },
+
+ student2 = {
+    name: "Reed",
+    regNo: "002"
+ },
+ 
+ student3 = {
+    name: "Reed",
+    regNo: "002"
+ };
+
+ let studentMap = new Map();
+
+ // The onjects above are the keys; & they are assigned to the values (course strings)
+ studentMap.set(student1, "computer science");
+ studentMap.set(student2, "mathematical biology");
+ studentMap.set(student3, "engineering physics");
+
+console.log(studentMap.size);
+console.log(studentMap.get(student1));
+studentMap.delete(student3);
+
+console.log(studentMap.has(student3));
