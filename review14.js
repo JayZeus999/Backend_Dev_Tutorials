2156175adb306111a34e704747a046606d96ce38
Falade Joseph <faladejoseph400@gmail.com>
2025-12-24 12:18:19 +0100

    Create new set and test methods

--- review14.js
+++ review14.js
@@ -4,3 +4,10 @@ hello = (a = 5, b = 5) => {
 };
 
 console.log(hello());
+
+// Create a set.
+let alphabets = new Set([1, 2, 2, 4, 16]);
+// console.log(alphabets.size)
+console.log(alphabets.has("a")); 
+console.log(alphabets) // 4 elements; only permits unique values. 
+
