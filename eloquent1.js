9303c9ed9e858ac2c4143c86a3954d609c35e040
Falade Joseph <faladejoseph400@gmail.com>
2025-12-25 23:28:58 +0100

    Create a reverse triangle

--- eloquent1.js
+++ eloquent1.js
@@ -1,9 +1,21 @@
 function hashtags () {
     let sevenTags = [];
     for (let i = 0; i < 7; i++) {
-        sevenTags += "#"
+        sevenTags += "#";
+        console.log(sevenTags);
     }
-    console.log(sevenTags)
+    
+    let arr = sevenTags.split("")
+    for (let i = 7; i > 0; i--){
+        arr.pop();
+        let string = arr;
+        console.log(string.join(""));
+    }
 }
 
 hashtags();
+
+// let x = "xxx"
+// let y = x.split("")
+// console.log(typeof(x))
+// console.log(typeof(y))
