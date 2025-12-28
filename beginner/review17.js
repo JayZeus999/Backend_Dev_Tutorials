4131617d9824af0d54a6dbfc25edda835dc4017e
Falade Joseph <faladejoseph400@gmail.com>
2025-12-28 13:32:14 +0100

    Search for matching patterns in words

--- beginner/review17.js
+++ beginner/review17.js
@@ -11,9 +11,20 @@ function sum(...args) {
 console.log(sum(1, 2, 3)); // Rest parameter has to be the last argument(s).
 
 
-// Regular Expressions.
+// Regular Expressions with ignore flag.
 console.log(/hello/i.test("Hello people")); 
 // OR
+
 let re = /dogs/i;
 let result = "Dogs are so cute and cuddly!";
 console.log(re.test(result));
+
+// RegExs with global flag.
+let result2 = result.match(/cute/ig);
+console.log(result2);
+
+// Regular expression character classes.
+var st = /w[aeo]rd/g;
+var statement = "He was lost for words, wards and weird werds."
+
+console.log(statement.match(st));
