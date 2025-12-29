b8c7989fc44a1dc7917d052cc460805e90a652fd
Falade Joseph <faladejoseph400@gmail.com>
2025-12-29 11:06:33 +0100

    Test out exact & range quantifiers

--- beginner/review19.js
+++ beginner/review19.js
@@ -8,3 +8,12 @@ console.log(/.$/.test(word2));
 // Word Boundaries.
 console.log("Hello, JSscript! *JSs".match(/\bJS\b/));
 console.log("Hello, JSscript! JS".match(/JS/));
+
+// Exact count {n} quantifier.
+let str = "ECMAScript 2020";
+let re = /\d{4}/;
+console.log(str.match(re));
+
+// Range {n,m} quantifier.
+let re2 = /\w{4,10}/;
+console.log(str.match(re2));
