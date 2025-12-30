e39aea59b066c661c013748ef56862e7ba50bbf9
Falade Joseph <faladejoseph400@gmail.com>
2025-12-30 17:35:21 +0100

    Construcct a promise & use it

--- beginner/review20.js
+++ beginner/review20.js
@@ -8,3 +8,30 @@ let arrow = (a, b, myCallBack) => {
 }
 
 arrow(1, 2, callBack);
+callBack(10000);
+
+// Anonymous function. Which is the anonymous f. above?
+setTimeout(function() {
+    console.log("hope");
+}, 1000);
+
+
+ // A Promise.
+ let buySomeGroceries = new Promise(function(myResolve, myReject) {
+    let supperIngredientsComplete = true;
+    if (supperIngredientsComplete) {
+        myResolve("Your supper is ready.")
+    } else {
+        myReject("The store is closed for the day, so no supper.")
+    }
+ });
+
+ // Using the Promise.
+ buySomeGroceries.then(
+    function(value) {
+        console.log("Here's your supper.", value);
+    },
+    function(reason) {
+        console.log("That's why we couldn't get the coffee.", reason);
+    }
+ )
