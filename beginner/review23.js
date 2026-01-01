94ed28e0967c205a00d1665698e6c951d0dc593d
Falade Joseph <faladejoseph400@gmail.com>
2026-01-01 13:05:59 +0100

    Use finnaly to ensure compeletion

--- beginner/review23.js
+++ beginner/review23.js
@@ -1,13 +1,38 @@
-function l(arg) {
-    console.log(arg);
-};
-
 // Exceptions.
 let num = 1;
 
 try {
-    // l(num.toPrecision(500)); 
+    // console.log(num.toPrecision(500)); 
     x = y + 999;
 } catch(error) {
     console.log(error.message);
+}
+
+// URI Errors
+try{
+    decodeURI("%%%");
+} catch(error) {
+    console.log(error.message)
+}
+
+
+let denom = 0;
+if (denom === 0) {
+    throw new RangeError("Attempted division by zero."); // can also be TypeError.
+}; 
+
+
+var a = 100;
+var b = 0;
+
+try {
+    if (b == 0) {
+        throw("Divide by zero error.");
+    } else {
+        var c = a / b;
+    }
+} catch (error) {
+    console.log("Error: " + error);
+} finally {
+    console.log("Code still runs in the end.");
 }
